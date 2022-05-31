1f23704217536eac92a11ef7f5a91731975eceaf
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-05-31 16:57:23 +0500

    Create app

--- next.config.js
+++ next.config.js
@@ -6,12 +6,14 @@ const nextConfig = {
   },
   images: {
     domains: [
-      'kajabi-storefronts-production.kajabi-cdn.com',
-      'upload.wikimedia.org',
-      'i.ytimg.com',
-      'angartwork.akamaized.net'
+      "kajabi-storefronts-production.kajabi-cdn.com",
+      "upload.wikimedia.org",
+      "i.ytimg.com",
+      "angartwork.akamaized.net",
+      "i.scdn.co",
+      "resources.tidal.com",
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
