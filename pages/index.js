eb256a6ac383f6f123495b30a23b1802f990d452
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-06-02 22:32:57 +0500

    test head

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Login from "../components/Login";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <audio
         id="audio-element"
         hidden
