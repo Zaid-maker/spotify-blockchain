18f7df16c3a39e44e757ff80eab661956e3848ef
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-06-03 17:34:38 +0000

    added back

--- components/Login.js
+++ components/Login.js
@@ -1,23 +1,23 @@
-import { useWallet } from "@solana/wallet-adapter-react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 //import { Payment } from "./Payment";
 
 const styles = {
   loginPage: `w-screen h-screen bg-white flex justify-center flex-col items-center`,
   text: `text-4xl text-black mb-10`,
-};
+}
 
 const Login = () => {
   //const wallet = useWallet();
 
   //if (wallet.connected) return <Payment />;
-  
+
   return (
     <div className={styles.loginPage}>
       <p className={styles.text}>Login to access this app.</p>
       <WalletMultiButton />
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
