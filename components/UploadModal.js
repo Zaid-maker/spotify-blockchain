d053eb3fffe5f946dd41de4e94403b04133f2a3c
Zaid-maker <pzhafeez@gmail.com>
2022-06-05 17:59:28 -0500

    Header fone

---
+++ components/UploadModal.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const UploadModal = () => {
+  return <div>UploadModal</div>
+}
+
+export default UploadModal
--- pages/homepage.js
+++ pages/homepage.js
@@ -1,17 +1,38 @@
+import { useState, useEffect } from 'react'
 import Nav from '../components/nav'
 import Activity from '../components/activity'
+import Playlist from '../components/playlist'
+import Header from '../components/Header'
+import PlayerControls from '../components/PlayerControls'
+import UploadModal from '../components/UploadModal'
 import Head from 'next/head'
 
 const HomePage = () => {
+  const [showUploadMusic, setShowUploadMusic] = useState(false)
+  const [title, setTitle] = useState('')
+  const [musicUrl, setMusicUrl] = useState('')
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    //getSongs().then((songs) => {
+    //  setSongs(songs)
+    //})
+  }, [])
+
   return (
-    <div className='flex'>
+    <div className="flex">
       <Head>
         <title>Spotify Blockchain - Homepage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <Nav />
-      <div className='w-full'>{/* Start coding here */}</div>
+      <div className="w-full">
+        <Header setShowUploadMusic={setShowUploadMusic} />
+        <Playlist songs={songs} />
+        <PlayerControls songs={songs} />
+        {showUploadMusic && <UploadModal />}
+      </div>
       <Activity />
     </div>
   )
