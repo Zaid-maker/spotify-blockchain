462014299527594af23248b7371755dcf810cd57
Zaid-maker <pzhafeez@gmail.com>
2022-06-04 23:02:52 -0500

    make the payment work

--- components/Payment.js
+++ components/Payment.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { getProgramInstance } from '../utils/utils'
 import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
 import { SOLANA_HOST } from '../utils/const'
 import { PublicKey } from '@solana/web3.js'
@@ -11,7 +12,7 @@ const { web3 } = anchor
 const { SystemProgram } = web3
 const utf8 = anchor.utils.bytes.utf8
 
-const defaultsAccounts = {
+const defaultAccounts = {
   tokenProgram: TOKEN_PROGRAM_ID,
   clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
   systemProgram: SystemProgram.programId,
@@ -27,17 +28,65 @@ const styles = {
 const Payment = () => {
   const wallet = useWallet()
   const connection = new anchor.web3.Connection(SOLANA_HOST)
-  //const program = getProgramInstance(connection, wallet)
+  const program = getProgramInstance(connection, wallet)
   const [payers, setPayers] = useState([])
   const [isPaid, setPaid] = useState(false)
+
+  useEffect(() => {
+    if (wallet.connected) getAllWallets()
+  }, [wallet.connected, isPaid])
+
+  const getAllWallets = async () => {
+    const payerList = await program.account.payerAccount.all()
+    setPayers(payerList)
+
+    payerList.forEach((payer) => {
+      if (payer.account.wallet.toBase58() == wallet.publicKey.toBase58())
+        setPaid(true)
+    })
+  }
+
+  const payClicked = async () => {
+    let [payerSigner] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode('payer'), wallet.publicKey.toBuffer()],
+      program.programId
+    )
+
+    let payerInfo
+
+    try {
+      payerInfo = await program.account.payAccount.fetch(payerSigner)
+    } catch (e) {
+      try {
+        await program.rpc.acceptPayment({
+          accounts: {
+            payerWallet: payerSigner,
+            receiver: new PublicKey(
+              'FdGbqLGZQgTpqXc1bKa41YsJTWHPxfwZKmTykjG6Jj1V'
+            ),
+            authority: wallet.publicKey,
+            ...defaultAccounts,
+          },
+        })
+      } catch (e) {
+        alert(e.message)
+      }
+    }
+  }
 
   if (isPaid) return <HomePage />
   return (
     <div className={styles.main}>
       <p className={styles.text}>Make Payment to use the service.</p>
       <div className={styles.buttons}>
-        <button className={styles.button}>Pay 0.1 SOL</button>
-        <button className={styles.button}>Update List</button>
+        <button
+          className={styles.button}
+          onClick={payClicked}
+          disabled={isPaid}
+        >
+          Pay 0.1 SOL
+        </button>
+        <button className={styles.button}>Verify Payment</button>
       </div>
     </div>
   )
--- utils/const.js
+++ utils/const.js
@@ -1,3 +1,10 @@
-import { clusterApiUrl } from "@solana/web3.js";
+import { clusterApiUrl, PublicKey } from '@solana/web3.js'
+import spotify from './spotify.json'
 
-export const SOLANA_HOST = clusterApiUrl("devnet");
+export const SOLANA_HOST = clusterApiUrl('devnet')
+
+export const STABLE_POOL_PROGRAM_ID = new PublicKey(
+  '5wSMdEYxSW7iB3rdE7c8yB3bqBUrVtvggjDdY5viyDLk'
+)
+
+export const STABLE_POOL_IDL = spotify
